2a2ecab295d731f7889e7cb969491aa6f032ef1b
Dhatri0612 <tripathi.dhatri0623@gmail.com>
2025-09-10 10:55:17 +0530

    Added exception folder

---
+++ Exception/MultipleCatchDemo.java
@@ -0,0 +1,16 @@
+
+
+public class MultipleCatchDemo {
+    public static void main(String[] args) {
+        int[] arr = null;
+        try {
+            arr = new int[]{10, 20, 30};
+            int index = 5;
+            System.out.println("Value at index " + index + ": " + arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
+}
